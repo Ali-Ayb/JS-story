580fa621fb9e28f67576f2cc8c0d4e6e6ac74ec6
Ali-Ayb <aliayoub.me1@gmail.com>
2023-02-26 14:05:20 +0200

    added specail character checker function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+function hasSpecialChar(password) {
+  const specialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+  return specialChars.test(password);
+}
+
 function checkPassword(password) {
   if (password.length < 8) {
     alert("Password should contain minimum of 8 characters");
+  }
+
+  if (hasSpecialChar(password)) {
+    alert("all good");
+  } else {
+    alert("Password should contain minimum of 1 special characters");
   }
 }
 
