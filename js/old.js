41115a6accd96741623b64a37ffeb170ced59780
Ali-Ayb <aliayoub.me1@gmail.com>
2023-02-27 01:19:50 +0200

    added merge sort algorithm

--- js/old.js
+++ js/old.js
@@ -1,9 +1,58 @@
-function addTenNumbers() {
-  const numbers = [];
-  for (let i = 0; i < 10; i++) {
-    const input = prompt(`Enter 10 numbers ${i + 1}:`);
-    numbers.push(parseInt(input));
+let numbers = [];
+// alert("Hello old but gold \nlets get started with some fun games old man");
+// for (let i = 0; i < 10; i++) {
+//   const input = prompt(`Enter 10 numbers ${i + 1}:`);
+//   numbers.push(parseInt(input));
+// }
+
+function merge(arr, l, m, r) {
+  var n1 = m - l + 1;
+  var n2 = r - m;
+  var L = new Array(n1);
+  var R = new Array(n2);
+  for (var i = 0; i < n1; i++) L[i] = arr[l + i];
+  for (var j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+  var i = 0;
+  var j = 0;
+  var k = l;
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+  while (j < n2) {
+    arr[k] = R[j];
+    j++;
+    k++;
   }
 }
 
-addTenNumbers();
+function mergeSort(arr, l, r) {
+  if (l >= r) {
+    return;
+  }
+  var m = l + parseInt((r - l) / 2);
+  mergeSort(arr, l, m);
+  mergeSort(arr, m + 1, r);
+  merge(arr, l, m, r);
+}
+let list = [1, 2, 4, 61, 8, 46, 1, 64];
+const unsorted_list = document.createElement("h1");
+unsorted_list.textContent = `unsorted numbers: ${list}`;
+document.body.appendChild(unsorted_list);
+
+mergeSort(list, 0, list.length - 1);
+
+const sorted_list = document.createElement("h1");
+sorted_list.textContent = `sorted numbers: ${list}`;
+document.body.appendChild(sorted_list);
